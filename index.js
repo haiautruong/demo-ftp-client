7767cdf0826db9c2e746ebf80deb9733f132d64d
Trương Thị Hải Âu <31470867+haiautruong@users.noreply.github.com>
2019-07-01 18:17:55 +0700

    demo download lasted version file

--- index.js
+++ index.js
@@ -1,44 +1,85 @@
 'use strict';
 var net = require("net");
 const FTP = require("jsftp");
-const SocksClient = require('socks').SocksClient;
 
+// const USER = "haiau123";
+// const PASSWORD = "123";
+// const HOST = 'localhost';
 
-const USER = "haiau123";
-const PASSWORD = "123";
-const HOST = '192.168.31.107'; // host proxy
-//const HOST = 'localhost';
+const USER = "duckha";
+const PASSWORD = "123456";
+const HOST = '192.168.31.107';
+const POST = 21;
 
-const POST = 80
-
-const option = {
-    user: USER,
-    pass: PASSWORD,
-    host: HOST,
-    port: POST,
-    createSocket: ({ port, host }, firstAction) => {
-        return net.createConnection({ port, host }, firstAction);
-    }
-
+const Option = {
+  user: USER,
+  pass: PASSWORD,
+  host: HOST,
+  port: POST,
+  createSocket: ({ port, host }, firstAction) => {
+    return net.createConnection({ port, host }, firstAction);
+  }
 }
-let ftp = new FTP(option);
-let str = "";
-ftp.get("test/haiau1.txt", async (err, socket) => {
-    if (err) {
-        console.log(err);
-      return;
-    }
-    console.log("-----------");
-    await socket.on("data", d => {
-      str += d.toString();
-      console.log("content", str);
-    });
-   
-    socket.on("close", hadErr => {
-      if (hadErr) {
-        console.error("There was an error retrieving the file.");
+
+let currTime = 0;
+
+function compare(a, b) {
+  if (a.time > b.time) {
+    return 1;
+  }
+  if (a.time < b.time) {
+    return -1;
+  }
+  return 0;
+}
+
+function downloadFile() {
+  let ftp = new FTP(Option);
+
+  ftp.ls("test/" + 'hai' + "*.txt", (err, res) => {
+    res.sort(compare);
+    console.log("----------------------------");
+    // res.forEach(file => console.log(file.time));
+    let lastestFile = res.map(e => {
+      if (e.time > currTime) {
+        return e;
       }
     });
-   
-    socket.resume();
+    console.log(lastestFile);
+    lastestFile.forEach(e => {
+      console.log("name: ", e.name);
+
+      let str = "";
+      ftp.get("test/" + e.name, (err, socket) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        socket.on("data", d => {
+          str += d.toString();
+          console.log("content-----------", str);
+        });
+        socket.on("close", hadErr => {
+
+          if (hadErr) {
+            console.error("There was an error retrieving the file.");
+          }
+        });
+
+        socket.resume();
+        ftp.raw("quit", (err, data) => {
+          currTime = e.time;
+          if (err) {
+            return console.error(err);
+          }
+
+          console.log("Bye!");
+        });
+
+      });
+    })
+
   });
+}
+
+setInterval(downloadFile, 5000);
