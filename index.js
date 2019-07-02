c0791e47df922e909fca5c236e85a0c944e8d149
hai au <haiau762@gmail.com>
2019-07-02 15:18:52 +0700

    update

--- index.js
+++ index.js
@@ -6,23 +6,12 @@ const FTP = require("jsftp");
 // const PASSWORD = "123";
 // const HOST = 'localhost';
 
-const USER = "duckha";
-const PASSWORD = "123456";
-const HOST = '192.168.31.107';
+const USER = 'haiau123';
+const PASSWORD = "haiau@@123";
+const HOST = 'ftp.drivehq.com';
 const POST = 21;
 
-const Option = {
-  user: USER,
-  pass: PASSWORD,
-  host: HOST,
-  port: POST,
-  createSocket: ({ port, host }, firstAction) => {
-    return net.createConnection({ port, host }, firstAction);
-  }
-}
-
 let currTime = 0;
-
 function compare(a, b) {
   if (a.time > b.time) {
     return 1;
@@ -33,53 +22,90 @@ function compare(a, b) {
   return 0;
 }
 
-function downloadFile() {
-  let ftp = new FTP(Option);
+function connect() {
+  const Option = {
+    user: USER,
+    pass: PASSWORD,
+    host: HOST,
+    port: POST,
+    createSocket: ({ port, host }, firstAction) => {
+      return net.createConnection({ port, host }, firstAction);
+    }
+  }
+  return new FTP(Option);
+}
 
-  ftp.ls("test/" + 'hai' + "*.txt", (err, res) => {
-    res.sort(compare);
-    console.log("----------------------------");
-    // res.forEach(file => console.log(file.time));
-    let lastestFile = res.map(e => {
-      if (e.time > currTime) {
-        return e;
+function disconnect(ftp) {
+  ftp.raw('quit', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      // console.log('disconnected');
+    }
+  })
+}
+
+function getList() {
+  return new Promise((resolve, reject) => {
+    let ftp = connect();
+    console.log('ftp-----------------', ftp);
+    ftp.ls('GroupRead/*' + 'au' + "*.txt", (err, res) => {
+      console.log('res--------------------', res);
+      console.log('err--------------------', err);
+      if (err) {
+        reject(err);
       }
-    });
-    console.log(lastestFile);
-    lastestFile.forEach(e => {
-      console.log("name: ", e.name);
+      else {
+        res.sort(compare);
+        resolve(res);
+        // disconnect(ftp);
+      }
+    })
+  })
+}
 
-      let str = "";
-      ftp.get("test/" + e.name, (err, socket) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
+function downFile(filename) {
+  return new Promise((resolve, reject) => {
+    let ftp = connect();
+    console.log('connected!!!!!!!!!!!');
+    ftp.get("GroupRead/" + filename, (err, socket) => {
+      if (err) {
+        console.log('err', err);
+        reject(err);
+      }
+      else {
         socket.on("data", d => {
-          str += d.toString();
-          console.log("content-----------", str);
+
+          d = d.toString();
+          resolve(d);
         });
         socket.on("close", hadErr => {
 
           if (hadErr) {
-            console.error("There was an error retrieving the file.");
+            reject(hadErr)
           }
         });
-
         socket.resume();
-        ftp.raw("quit", (err, data) => {
-          currTime = e.time;
-          if (err) {
-            return console.error(err);
-          }
-
-          console.log("Bye!");
-        });
-
-      });
+        // disconnect(ftp);
+      }
     })
-
-  });
+  })
 }
 
-setInterval(downloadFile, 5000);
+function downloadFile() {
+  getList().then(listFile => {
+    console.log('listFile-------------------', listFile);
+    listFile.map(file => {
+      downFile(file.name).then(data => {
+        console.log('data', data);
+        console.log('typeof data', typeof data);
+        console.log('updated data', data.split('\n'));
+      });
+    })
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+downloadFile();
